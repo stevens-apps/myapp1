98308a5a9dbf36bd241baa0bdd78cb48a61d135d
stevens-apps <stevens_apps@yahoo.com>
2016-01-19 18:30:16 -0800

    ''

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,28 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("charge", function(request, response) {
+  // Set your secret key: remember to change this to your live secret key in production
+  // See your keys here https://dashboard.stripe.com/account/apikeys
+  Stripe.apiKey = "sk_test_gbTLQukKCbf32YaWJxE7wtVS";
+
+  // Get the credit card details submitted by the form
+  String token = request.getParameter("stripeToken");
+  String amount = request.getParameter("amount");
+
+  // Create the charge on Stripe's servers - this will charge the user's card
+  try {
+    Map<String, Object> chargeParams = new HashMap<String, Object>();
+    chargeParams.put("amount", amount); // amount in cents, again
+    chargeParams.put("currency", "usd");
+    chargeParams.put("source", token);
+    chargeParams.put("description", "Example charge");
+
+    Charge charge = Charge.create(chargeParams);
+  } catch (CardException e) {
+    response.console.error("Card declined");
+    // The card has been declined
+  }
+  response.success("Card Charged");
+});
